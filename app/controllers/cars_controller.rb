7ec18d434ae419e782a616e566e1fec064794d0c
Leonardo Barbieri Bedendo <leonardobdn@gmail.com>
2021-02-03 20:59:30 -0300

    Add form for creating cars

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,12 @@
 class CarsController < ApplicationController
+    def create
+        @automaker = Automaker.find(params[:automaker_id])
+        @car = @automaker.cars.create(car_params)
+        redirect_to automaker_path(@automaker)
+    end
+
+    private
+        def car_params
+            params.require(:car).permit(:name, :modelYear)
+        end
 end
