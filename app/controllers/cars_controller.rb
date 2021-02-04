5b70d47b9832a8b039d4898b45e2c3ae04b33d08
Leonardo Barbieri Bedendo <leonardobdn@gmail.com>
2021-02-03 21:29:16 -0300

    Add new fields on cars form

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -7,6 +7,6 @@ class CarsController < ApplicationController
 
     private
         def car_params
-            params.require(:car).permit(:name, :model_year)
+            params.require(:car).permit(:name, :model_year, :color, :product_code)
         end
 end
