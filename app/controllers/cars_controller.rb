29216948f74ac5d0e32ec3d784e7b8f9b1e234fb
Leonardo Barbieri Bedendo <leonardobdn@gmail.com>
2021-02-08 22:48:10 -0300

    Add path to list cars in a separate view

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -5,6 +5,10 @@ class CarsController < ApplicationController
         redirect_to automaker_path(@automaker)
     end
 
+    def index
+        @cars = Car.joins("JOIN automakers ON automakers.id = cars.automaker_id").order("automakers.name, cars.name")
+    end
+
     private
         def car_params
             params.require(:car).permit(:name, :model_year, :color, :product_code)
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   resources :automakers do
     resources :cars
   end
+
+  resources :cars
+  resources :collections
+  resources :brands
 end
