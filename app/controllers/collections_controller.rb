330a71f84e91c7ab5780e9a29778309b029609c0
Leonardo Barbieri Bedendo <leonardobdn@gmail.com>
2021-02-18 22:21:25 -0300

    Create form for new collection

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -2,4 +2,27 @@ class CollectionsController < ApplicationController
     def index
         @collections = Collection.all
     end
+
+    def show
+        @collection = Collection.find(params[:id])
+    end
+
+    def new
+        @collection = Collection.new
+    end
+
+    def create
+        @collection = Collection.new(collection_params)
+
+        if @collection.save
+            redirect_to action: "index"
+        else
+            render :new
+        end
+    end
+
+    private
+        def collection_params
+            params.require(:collection).permit(:name, :year)
+        end
 end
